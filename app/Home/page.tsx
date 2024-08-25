07e0c559ed30f2b8dbd9046114b8d84be94ef45a
Stefanos2002 <kalouliss@gmail.com>
2024-08-25 21:51:12 +0300

    .

---
+++ app/Home/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="flex items-center justify-center h-screen w-full">
+      <span className="text-2xl text-black">
+        BLLLYYYYAAAAAAAAAAAAAAAAAAAAAATTTTTTTTTTTTTTTTTTT!!!
+      </span>
+    </div>
+  );
+}
